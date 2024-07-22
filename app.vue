306bb6fe00973db3061a50f85d94a2b20fc97a7f
Alexa <adarrington@ewu.edu>
2024-07-22 13:19:03 -0700

    Added vuetify packages and started app bar

--- app.vue
+++ app.vue
@@ -1,6 +1,14 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <NuxtLayout>
+    <v-app>
+      <NuxtPage />
+      <v-app-bar :elevation="2">
+  <template v-slot:prepend>
+    <v-app-bar-nav-icon></v-app-bar-nav-icon>
+  </template>
+
+  <v-app-bar-title>Application Bar</v-app-bar-title>
+</v-app-bar>
+    </v-app>
+  </NuxtLayout>
 </template>
