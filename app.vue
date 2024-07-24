0957619ca71a68c3ba39c5b1f29b7cdd5f3365fa
Alexa <adarrington@ewu.edu>
2024-07-24 11:17:02 -0700

    Added some scaffolding and beginning layout

--- app.vue
+++ app.vue
@@ -1,58 +1,5 @@
 <template>
   <NuxtLayout>
-    <v-app>
-      <NuxtPage />
-<template>
-  <v-card color="grey-lighten-3">
-    <v-layout>
-      <v-app-bar
-        color="white"
-      >
-        <template v-slot:image>
-          <v-img
-            gradient="to top right, rgba(19,84,122,.8), rgba(128,208,199,.8)"
-          ></v-img>
-        </template>
-
-        <template>
-          <v-app-bar-nav-icon></v-app-bar-nav-icon>
-        </template>
-
-        <v-app-bar-title>Alexa's Resume App</v-app-bar-title>
-
-        <v-spacer></v-spacer>
-        <v-btn >
-          Projects
-        </v-btn>
-        <v-btn >
-          Resume
-        </v-btn>
-
-        <v-btn icon>
-          <v-icon>mdi-dots-vertical</v-icon>
-        </v-btn>
-      </v-app-bar>
-
-      <v-main>
-        <v-container fluid>
-          <v-row dense>
-            <v-col
-              v-for="n in 2"
-              :key="n"
-              cols="12"
-            >
-              <v-card
-                :subtitle="`Subtitle for Content ${n}`"
-                :title="`Content ${n}`"
-                text="Lorem ipsum dolor sit amet consectetur, adipisicing elit.?"
-              ></v-card>
-            </v-col>
-          </v-row>
-        </v-container>
-      </v-main>
-    </v-layout>
-  </v-card>
-</template>
-    </v-app>
+    <NuxtPage />
   </NuxtLayout>
 </template>
---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+    <v-card
+      color=#C2185B
+      class="mx-auto mt-8 w-75 pa-8 rounded text-center"
+      elevation="4"
+    >
+      <v-card-title class="text-wrap">
+        Alexas resume gallery 
+      </v-card-title>
+      <v-card-subtitle class="text-wrap">
+        Welcome 
+      </v-card-subtitle>
+      <v-card-actions class="justify-center mx-2 my-4">
+        <v-btn color=#F06292 class="bg-#F06292" to="/resume">
+          Resume
+        </v-btn>
+        <v-btn color=#F06292 class="bg-#F06292" to="/projects">
+          Projects
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </template>
