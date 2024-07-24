577777868a5c104704ac40e726a635ab1be3bca1
Alexa <adarrington@ewu.edu>
2024-07-24 13:09:22 -0700

    Added more pages and tool bar

---
+++ pages/about.vue
@@ -0,0 +1,12 @@
+<template>
+    <v-card
+      color="#C2185B"
+      class="mx-auto mt-8 w-75 pa-8 rounded text-center"
+      elevation="4"
+    >
+      <v-card-title class="text-wrap"> Here is my about page </v-card-title>
+      <v-card-subtitle class="text-wrap"> blahhhhh </v-card-subtitle>
+      <v-card-actions class="justify-center mx-2 my-4"> </v-card-actions>
+    </v-card>
+  </template>
+  
--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,21 @@
 <template>
-    <v-card
-      color=#C2185B
-      class="mx-auto mt-8 w-75 pa-8 rounded text-center"
-      elevation="4"
-    >
-      <v-card-title class="text-wrap">
-        Alexas resume gallery 
-      </v-card-title>
-      <v-card-subtitle class="text-wrap">
-        Welcome 
-      </v-card-subtitle>
-      <v-card-actions class="justify-center mx-2 my-4">
-        <v-btn color=#F06292 class="bg-#F06292" to="/resume">
-          Resume
-        </v-btn>
-        <v-btn color=#F06292 class="bg-#F06292" to="/projects">
-          Projects
-        </v-btn>
-      </v-card-actions>
-    </v-card>
-  </template>
+    <v-toolbar>
+        <v-btn to="/"> Home </v-btn>
+        <v-btn to="/about"> About </v-btn>
+
+
+      <v-toolbar-title class="text-center"> Alexa Darrington </v-toolbar-title>
+      <v-btn to="/resume"> Resume </v-btn>
+      <v-btn to="/projects"> Projects </v-btn>
+    </v-toolbar>
+  <v-card
+    color="#C2185B"
+    class="mx-auto mt-8 w-75 pa-8 rounded text-center"
+    elevation="4"
+  >
+    <v-card-title class="text-wrap"> Alexas resume gallery </v-card-title>
+    <v-card-subtitle class="text-wrap"> Welcome </v-card-subtitle>
+    <v-card-actions class="justify-center mx-2 my-4">
+    </v-card-actions>
+  </v-card>
+</template>
