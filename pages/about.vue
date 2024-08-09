977103c50d14e0682dbd3c4add8f1bcdd4022e36
Alexa <adarrington@ewu.edu>
2024-08-09 15:24:29 -0700

    Added table for languages and added project gallery

--- pages/about.vue
+++ pages/about.vue
@@ -1,18 +1,102 @@
 <template>
-  <v-card
-    color="primary"
-    class="mx-auto mt-8 w-75 pa-8 rounded text-center"
-    elevation="4"
-  >
-    <v-card-title class="custom-font text-wrap"> ABOUT ALEXA </v-card-title>
-    <v-card-subtitle class="custom-font text-wrap">
-      I am currently a senior at Eastern Washington University in Spokane,WA
-    </v-card-subtitle>
-    <v-btn to="/"> Home </v-btn>
+    <v-card
+      color="primary"
+      class="mx-auto mt-8 w-75 pa-8 rounded text-center"
+      elevation="4"
+    >
+      <v-card-title class="custom-font text-wrap"> ABOUT ALEXA </v-card-title>
+      <v-card-subtitle class="custom-font text-wrap">
+        I am currently a senior at Eastern Washington University in Spokane, WA. I
+        am looking for full time roles in software development or cyber security.
+        I am passionate about using technology to solve real world problems and I
+        am always looking for new opportunities to learn and grow.
+      </v-card-subtitle>
+      <v-card-title class="custom-font text-wrap"> Stacks Technologies Alexa Uses: </v-card-title>
+  
+      <!-- Table with padding and margin -->
+      <v-container>
+        <v-table >
+          <thead>
+            <tr>
+              <th class="text-left">Languages/Tools</th>
+              <th class="text-left">Proficiency Level</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="item in desserts" :key="item.name" class="pa-2">
+              <td>{{ item.name }}</td>
+              <td>{{ item.level }}</td>
+            </tr>
+          </tbody>
+        </v-table>
+      </v-container>
+  
+      <v-container class="mt-4">
+        <v-card-subtitle class="custom-font text-left">
+          <strong>The scale ranges from proficiency levels 1-5:</strong>
+          <ul>
+            <li><strong>1 -</strong> Fundamental Awareness (basic knowledge).</li>
+            <li><strong>2 -</strong> Novice (limited experience).</li>
+            <li><strong>3 -</strong> Intermediate (practical application).</li>
+            <li><strong>4 -</strong> Advanced (applied theory).</li>
+            <li><strong>5 -</strong> Expert (recognized authority).</li>
+          </ul>
+        </v-card-subtitle>
+      </v-container>
+  
+      <!-- Home button with margin top -->
+      <v-container class="mt-4">
+        <v-btn to="/"> Home </v-btn>
+      </v-container>
+  
+      <v-card-actions class="justify-center mx-2 my-4"> </v-card-actions>
+    </v-card>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        desserts: [
+          {
+            name: 'Java',
+            level: 3,
+          },
+          {
+            name: 'Python',
+            level: 3,
+          },
+          {
+            name: 'C#',
+            level: 3,
+          },
+          {
+            name: 'Front End (HTML, CSS, JS, TS)',
+            level: 3,
+          },
+          {
+            name: 'Linux',
+            level: 4,
+          },
+          {
+            name: 'AWS Services',
+            level: 4,
+          },
+          {
+            name: 'GitHub',
+            level: 4,
+          },
+          {
+            name: 'Bash Scripting',
+            level: 2,
+          },
+        ],
+      };
+    },
+  };
+  </script>
 
-    <v-card-actions class="justify-center mx-2 my-4"> </v-card-actions>
-  </v-card>
-</template>
+
 
 <style scoped>
 @font-face {
--- pages/projects.vue
+++ pages/projects.vue
@@ -1,15 +1,45 @@
 <template>
-  <v-card
-    color="primary"
-    class="mx-auto mt-8 w-75 pa-8 rounded text-center"
-    elevation="4"
-  >
-    <v-card-title class="custom-font text-wrap"> PROJECTS & INVOLVEMENTS </v-card-title>
-    <v-card-subtitle class="text-wrap"> blahhhhh </v-card-subtitle>
-    <v-card-actions class="justify-center mx-2 my-4"> </v-card-actions>
-    <v-btn to="/"> Home </v-btn>
-  </v-card>
-</template>
+    <v-card
+      color="primary"
+      class="mx-auto mt-8 w-75 pa-8 rounded text-center"
+      elevation="4"
+    >
+      <v-card-title class="custom-font text-wrap"> PROJECTS & INVOLVEMENTS </v-card-title>
+  
+      <v-carousel show-arrows="hover">
+        <v-carousel-item
+          src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+          cover
+          @click="goToProject('project1')"
+        ></v-carousel-item>
+  
+        <v-carousel-item
+          src="https://cdn.vuetifyjs.com/images/cards/hotel.jpg"
+          cover
+          @click="goToProject('project2')"
+        ></v-carousel-item>
+  
+        <v-carousel-item
+          src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+          cover
+          @click="goToProject('project3')"
+        ></v-carousel-item>
+      </v-carousel>
+  
+      <v-card-actions class="justify-center mx-2 my-4"> </v-card-actions>
+      <v-btn to="/"> Home </v-btn>
+    </v-card>
+  </template>
+  
+  <script>
+  export default {
+    methods: {
+      goToProject(path) {
+        this.$router.push(path);
+      },
+    },
+  };
+  </script>
 
 
 <style scoped>
